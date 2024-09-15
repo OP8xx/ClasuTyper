1ff7ad97f5b5bda16eaa0f6ebf5b998b1e4915d3
op8xx <oukkus@icloud.com>
2024-09-15 15:39:01 +0300

    changed the font and modified index meta tags

--- src/App.jsx
+++ src/App.jsx
@@ -313,9 +313,9 @@ function App() {
           <p>
             ClasuTyperissa testaat kirjoitusnopeuttasi Clasun opettajien sanomien lausahdusten avulla!
             <hr style={{border: `1px solid ${themeFontColor}`}}/>
-            Kyseessä on harrastelumielessä tehty avoimen lähdekoodin koodausprojekti. Sen tarkoituksena ei ole loukata ketään clasun kunnioitettavista opettajista  &lt;3
+            Kyseessä on harrastelumielessä tehty koodausprojekti. Sen tarkoituksena ei ole loukata ketään clasun kunnioitettavista opettajista  &lt;3
             <hr style={{border: `1px solid ${themeFontColor}`}}/>
-            Huom. Peli on vasta ensimmäisessä versiossaan, jonka vuoksi bugit ovat yleisiä. Kiitos ymmärryksestä!
+            Huom. Peli on vasta ensimmäisessä versiossaan, jonka vuoksi bugit saattavat olla yleisiä. Kiitos ymmärryksestä!
           </p>
       </div>
 
@@ -520,19 +520,6 @@ function App() {
 
       <footer>
         <div className='footer-icons'>
-          <a href="https://github.com/OP8xx/ClasuTyper" target='_blank'>
-            <svg
-              className='github-icon' 
-              fill={themeFontColor} 
-              width={25} height={25} 
-              viewBox="0 0 24 24" 
-              xmlns="http://www.w3.org/2000/svg" 
-              data-name="Layer 1"
-              href='https://github.com/OP8xx/ClasuTyper' target='_blank'
-            >
-              <path d="M12,2.2467A10.00042,10.00042,0,0,0,8.83752,21.73419c.5.08752.6875-.21247.6875-.475,0-.23749-.01251-1.025-.01251-1.86249C7,19.85919,6.35,18.78423,6.15,18.22173A3.636,3.636,0,0,0,5.125,16.8092c-.35-.1875-.85-.65-.01251-.66248A2.00117,2.00117,0,0,1,6.65,17.17169a2.13742,2.13742,0,0,0,2.91248.825A2.10376,2.10376,0,0,1,10.2,16.65923c-2.225-.25-4.55-1.11254-4.55-4.9375a3.89187,3.89187,0,0,1,1.025-2.6875,3.59373,3.59373,0,0,1,.1-2.65s.83747-.26251,2.75,1.025a9.42747,9.42747,0,0,1,5,0c1.91248-1.3,2.75-1.025,2.75-1.025a3.59323,3.59323,0,0,1,.1,2.65,3.869,3.869,0,0,1,1.025,2.6875c0,3.83747-2.33752,4.6875-4.5625,4.9375a2.36814,2.36814,0,0,1,.675,1.85c0,1.33752-.01251,2.41248-.01251,2.75,0,.26251.1875.575.6875.475A10.0053,10.0053,0,0,0,12,2.2467Z"/>
-            </svg>
-          </a>
           <svg onClick={() => setPrivacyOpen(!privacyOpen)} className='privacy-icon' fill={themeFontColor} width={25} height={25} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24"><path d="M17,9V7c0-2.8-2.2-5-5-5S7,4.2,7,7v2c-1.7,0-3,1.3-3,3v7c0,1.7,1.3,3,3,3h10c1.7,0,3-1.3,3-3v-7C20,10.3,18.7,9,17,9z M9,7c0-1.7,1.3-3,3-3s3,1.3,3,3v2H9V7z"/></svg>
         </div>
         <div className='version'>
